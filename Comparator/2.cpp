3a84be1ff67af8e51539a6c0d1b947a0be336aad
akshita1204 <akshitatyagi04@gmail.com>
2025-05-15 16:29:09 +0530

    Commit

---
+++ Comparator/2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ct[100]={0};
+bool cmp(int a,int b)
+{
+  if(ct[a]==ct[b]) return a>b;
+  else return ct[a]>ct[b];
+}
+
+int main()
+{
+ int n;
+ cin>>n;
+ vector<int>v(n);
+ for(int i=0;i<n;i++) cin>>v[i];
+ for(int i=0;i<n;i++)
+ {
+   ct[i]++;
+ }
+ sort(v.begin(),v.end(),cmp);
+ for(int i=0;i<n;i++)
+ {
+   cout<<v[i]<<" ";
+ }
+}
+
+//Need to correct 
